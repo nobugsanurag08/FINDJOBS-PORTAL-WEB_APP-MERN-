0ebcee812260a1c563df76d64ead31daafe83f5e
Anurag Kumar <72593980+nobugsanurag08@users.noreply.github.com>
2023-12-18 12:41:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 dbconnect();
 async function dbconnect() {
     try{
-        await mongoose.connect('mongodb+srv://anuragnitp:anurag12345@cluster0.o68pw.mongodb.net/FindJob',{useNewUrlParser: true});
+        await mongoose.connect('mongodb+srv://"*********":"*************"@cluster0.o68pw.mongodb.net/FindJob',{useNewUrlParser: true});
         console.log("MongoDB connection Successfull..");
     }catch(err){
         console.log("Connection Failed!!");
